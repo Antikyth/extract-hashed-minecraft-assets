d357e7dfb53b3e6f3bbe2ab0d2264aa1fc9a3cbc
Zoe <104020300+Antikyth@users.noreply.github.com>
2025-03-26 19:28:17 +1300

    Fix `.minecraft` folder location on Linux

--- src/main.rs
+++ src/main.rs
@@ -66,7 +66,7 @@ fn minecraft_dir() -> Option<PathBuf> {
 
 #[cfg(not(any(target_os = "windows", target_os = "macos")))]
 fn minecraft_dir() -> Option<PathBuf> {
-    dirs::data_dir()
+    dirs::home_dir()
         .map(|path| path.join(".minecraft"))
         .filter(|path| path.is_dir())
 }
